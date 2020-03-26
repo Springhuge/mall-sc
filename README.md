d79adc93d100d9a6f9679acc7113e14a4d36a171
root <529581062@qq.com>
2020-03-26 09:38:31 +0800

    搭建 mbg 环境 并生成 mapper model

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mall-sc
 电商后台 Spring Cloud
+
+##目录
+mall-common 项目公共 返回状态 数据 封装
+
---
+++ mall-mbg/src/main/java/com/jihu/mall/mapper/PmsProductLadderMapper.java
@@ -0,0 +1,30 @@
+package com.jihu.mall.mapper;
+
+import com.jihu.mall.model.PmsProductLadder;
+import com.jihu.mall.model.PmsProductLadderExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface PmsProductLadderMapper {
+    long countByExample(PmsProductLadderExample example);
+
+    int deleteByExample(PmsProductLadderExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(PmsProductLadder record);
+
+    int insertSelective(PmsProductLadder record);
+
+    List<PmsProductLadder> selectByExample(PmsProductLadderExample example);
+
+    PmsProductLadder selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") PmsProductLadder record, @Param("example") PmsProductLadderExample example);
+
+    int updateByExample(@Param("record") PmsProductLadder record, @Param("example") PmsProductLadderExample example);
+
+    int updateByPrimaryKeySelective(PmsProductLadder record);
+
+    int updateByPrimaryKey(PmsProductLadder record);
+}
