811cbe45713b81062600bd8e501047846b93c8cf
root <529581062@qq.com>
2020-03-29 13:47:35 +0800

    完成ums 菜单列表模块

--- mall-ums/src/main/java/com/jihu/mall/ums/controller/MenuController.java
+++ mall-ums/src/main/java/com/jihu/mall/ums/controller/MenuController.java
@@ -1,14 +1,14 @@
 package com.jihu.mall.ums.controller;
 
+import com.jihu.mall.common.api.CommonPage;
 import com.jihu.mall.common.api.CommonResult;
+import com.jihu.mall.ums.model.UmsMenu;
 import com.jihu.mall.ums.model.response.UmsMenuNode;
 import com.jihu.mall.ums.service.MenuService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,63 @@ public class MenuController {
     public CommonResult getAllMenu(){
         List<UmsMenuNode> menuNodes = menuService.treeList();
         return CommonResult.success(menuNodes);
+    }
+
+    @ApiOperation("分页查询后台菜单")
+    @GetMapping("/list/{parentId}")
+    public CommonResult list(@PathVariable("parentId") Long parentId,
+                             @RequestParam(value = "pageNum",defaultValue = "1") Integer  pageNum,
+                             @RequestParam(value = "pageSize",defaultValue = "5") Integer  pageSize){
+        List<UmsMenu> menus = menuService.list(parentId,pageNum,pageSize);
+        return CommonResult.success(CommonPage.restPage(menus));
+    }
+
+    @ApiOperation("根据ID获取菜单详情")
+    @GetMapping("/{id}")
+    public CommonResult getItem(@PathVariable("id") Long id){
+        UmsMenu menu = menuService.getItem(id);
+        return CommonResult.success(menu);
+    }
+
+    @ApiOperation("添加后台菜单")
+    @PostMapping("/create")
+    public CommonResult create(@RequestBody UmsMenu menu){
+        int count = menuService.create(menu);
+        if(count > 0){
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("/修改后台菜单")
+    @PostMapping("/update/{id}")
+    public CommonResult update(@PathVariable("id") Long menuId,@RequestBody UmsMenu menu){
+        int count = menuService.update(menuId, menu);
+        if (count > 0) {
+            return CommonResult.success(count);
+        } else {
+            return CommonResult.failed();
+        }
+    }
+
+    @ApiOperation("根据id删除菜单")
+    @PostMapping("/delete/{id}")
+    public CommonResult delete(@PathVariable("id") Long id){
+        int count = menuService.delete(id);
+        if(count > 0){
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
+    }
+
+    @ApiOperation("修改菜单显示状态")
+    @PostMapping("/updateHidden/{id}")
+    public CommonResult updateHidden(@PathVariable("id") Long id,
+                                     @RequestParam("hidden") Integer hidden){
+        int count = menuService.updateHidden(id,hidden);
+        if(count > 0){
+            return CommonResult.success(count);
+        }
+        return CommonResult.failed();
     }
 }
--- mall-ums/src/main/java/com/jihu/mall/ums/service/MenuService.java
+++ mall-ums/src/main/java/com/jihu/mall/ums/service/MenuService.java
@@ -1,5 +1,6 @@
 package com.jihu.mall.ums.service;
 
+import com.github.pagehelper.PageHelper;
 import com.jihu.mall.ums.example.UmsMenuExample;
 import com.jihu.mall.ums.mapper.UmsMenuMapper;
 import com.jihu.mall.ums.model.UmsMenu;
@@ -8,7 +9,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Year;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service("menuService")
@@ -33,5 +37,63 @@ public class MenuService {
                 .map(subMenu -> covertMenuNode(subMenu,menuList)).collect(Collectors.toList());
         node.setChildren(result);
         return node;
+    }
+
+    public List<UmsMenu> list(Long parentId, Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        UmsMenuExample example = new UmsMenuExample();
+        example.setOrderByClause("sort desc");
+        example.createCriteria().andParentIdEqualTo(parentId);
+        return menuMapper.selectByExample(example);
+    }
+
+    public UmsMenu getItem(Long id) {
+        return menuMapper.selectByPrimaryKey(id);
+    }
+
+    public int create(UmsMenu menu) {
+        menu.setCreateTime(new Date());
+        updateLevel(menu);
+        return menuMapper.insert(menu);
+    }
+
+    private void updateLevel(UmsMenu menu) {
+        if(menu.getParentId() == 0){
+            //没有父菜单设为一级菜单
+            menu.setLevel(0);
+        }else{
+            //有父菜单，根据父菜单level+1
+            UmsMenu parentMenu = menuMapper.selectByPrimaryKey(menu.getParentId());
+            if(Optional.ofNullable(parentMenu).isPresent()) {
+                menu.setLevel(parentMenu.getLevel() + 1);
+            }else{
+                menu.setLevel(0);
+            }
+        }
+
+    }
+
+    public int update(Long menuId, UmsMenu menu) {
+        UmsMenu temp = menuMapper.selectByPrimaryKey(menuId);
+        if(Optional.ofNullable(temp).isPresent()){
+            menu.setId(menuId);
+            updateLevel(menu);
+            return menuMapper.updateByPrimaryKey(menu);
+        }
+        return 0;
+    }
+
+    public int delete(Long id) {
+        return menuMapper.deleteByPrimaryKey(id);
+    }
+
+    public int updateHidden(Long id, Integer hidden) {
+        UmsMenu temp = menuMapper.selectByPrimaryKey(id);
+        if(Optional.ofNullable(temp).isPresent()){
+            temp.setHidden(hidden);
+            updateLevel(temp);
+            return menuMapper.updateByPrimaryKey(temp);
+        }
+        return 0;
     }
 }
